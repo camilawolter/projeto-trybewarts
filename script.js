6a23f29ca905684e67266f52c7b703d3cdf5aacb
Camila Wolter <camila0wolter@gmail.com>
2022-06-19 12:26:55 -0300

    arrumando os erros do lint

--- script.js
+++ script.js
@@ -16,17 +16,16 @@ buttonEntrar.addEventListener('click', alerta);
 // desabilitando o botão enviar
 const buttonEnviar = document.getElementById('submit-btn');
 const checkbox = document.getElementById('agreement');
- 
+
 buttonEnviar.disabled = true;
- 
+
 // habilitar o botão enviar só quando tiver checando
 function verificarEstado() {
- if (checkbox.checked) {
-   buttonEnviar.disabled = false;
- } else {
-   buttonEnviar.disabled = true;
- }
+  if (checkbox.checked) {
+    buttonEnviar.disabled = false;
+  } else {
+    buttonEnviar.disabled = true;
+  }
 }
- 
+
 checkbox.addEventListener('click', verificarEstado);
- 
