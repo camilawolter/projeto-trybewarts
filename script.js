12f430f66f6ac049994a04c21e9272f614c5252b
Camila Wolter <camila0wolter@gmail.com>
2022-06-19 12:16:14 -0300

    requisito 18

--- script.js
+++ script.js
@@ -12,3 +12,21 @@ function alerta() {
 }
 
 buttonEntrar.addEventListener('click', alerta);
+
+// desabilitando o botão enviar
+const buttonEnviar = document.getElementById('submit-btn');
+const checkbox = document.getElementById('agreement');
+ 
+buttonEnviar.disabled = true;
+ 
+// habilitar o botão enviar só quando tiver checando
+function verificarEstado() {
+ if (checkbox.checked) {
+   buttonEnviar.disabled = false;
+ } else {
+   buttonEnviar.disabled = true;
+ }
+}
+ 
+checkbox.addEventListener('click', verificarEstado);
+ 
