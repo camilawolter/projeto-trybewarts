26b33b3ee748bc127463a0b13351c6ef290f1e55
Camila Wolter <camila0wolter@gmail.com>
2022-06-21 11:24:32 -0300

    arrumando lint e adicionando requisito 21

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const emailAvaliacao = document.getElementById('input-email');
+const nome = document.getElementById('input-name');
+const sobrenome = document.getElementById('input-lastname');
+const house = document.getElementById('house');
+const formAvaliacao = document.getElementById('evaluation-form');
+const resultado = document.getElementById('form-data');
+
 // criando alerta ao digitar o e-mail e senha
 const headerEmail = document.getElementById('header-email');
 const senha = document.getElementById('senha');
@@ -45,3 +52,24 @@ function contador() {
 textArea.addEventListener('keyup', contador);
 
 // substituindo o formulário pelas informações
+function subsFormAvaliacao(event) {
+  event.preventDefault();
+
+  const familia = document.querySelector('input[name=family]:checked');
+  const conteudos = document.querySelectorAll('.subject:checked');
+  const nota = document.querySelector('input[name=rate]:checked');
+
+  formAvaliacao.style.display = 'none';
+
+  resultado.innerText = `
+    Nome: ${nome.value} ${sobrenome.value}
+    Email: ${emailAvaliacao.value}
+    Casa: ${house.value}
+    Família: ${familia.value}
+    Matérias: ${[...conteudos].map((conteudo) => conteudo.value).join(', ')}
+    Avaliação:  ${nota.value}
+    Observações: ${textArea.value}
+  `;
+}
+
+formAvaliacao.addEventListener('submit', subsFormAvaliacao);
